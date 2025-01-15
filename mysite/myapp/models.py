840a17a246b3af2cec972eaa152af1a65175d199
VincentVisage <144054905+VincentVisage@users.noreply.github.com>
2025-01-15 23:54:52 +0300

    Init

---
+++ mysite/myapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    flats = models.FloatField()
+    calories = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
