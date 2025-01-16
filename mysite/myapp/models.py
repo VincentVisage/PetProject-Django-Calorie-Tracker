27ef61343e853dc44ce4d0e39226f562c47d957f
VincentVisage <144054905+VincentVisage@users.noreply.github.com>
2025-01-16 22:51:34 +0300

    Final

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,3 +15,6 @@ class Food(models.Model):
         return self.name
     
 
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
